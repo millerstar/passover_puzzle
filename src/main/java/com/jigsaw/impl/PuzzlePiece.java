d0835c78c0a2aac822fe90718a4324516bae8f20
Davidson, Matvey (md9897) <md9897@intl.att.com>
2018-03-27 19:04:51 +0300

    updated class PuzzlePiece

--- src/main/java/com/jigsaw/impl/PuzzlePiece.java
+++ src/main/java/com/jigsaw/impl/PuzzlePiece.java
@@ -1,19 +1,16 @@
 package com.jigsaw.impl;
 
 public class PuzzlePiece {
-	
-	//Private class members
-	private int TL,TR,BL,BR;
-	private int PazzlePieceID = 0;
-	
-	
-	//Ctors
-	public PuzzlePiece(int TL, int TR, int BL, int BR) {
-		this.TL = TL;
-		this.TR = TR;
-		this.BL = BL;
-		this.BR = BR;
-		PazzlePieceID++;
-	}
+
+	private int topLeft, topRight, bottomLeft, bottomRight;
+	private int pieceID;
+
+    public PuzzlePiece(int topLeft, int topRight, int bottomLeft, int bottomRight, int pieceID) {
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+        this.pieceID = pieceID;
+    }
 
 }
