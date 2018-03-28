0f520372bd6d4501654817420e1e06a4cc70084a
idan <idan006@gmail.com>
2018-03-28 15:08:53 +0300

    Added isValidPiece in PuzzlePieceValidator

--- src/main/java/com/jigsaw/impl/PuzzlePiece.java
+++ src/main/java/com/jigsaw/impl/PuzzlePiece.java
@@ -3,6 +3,7 @@ package com.jigsaw.impl;
 public class PuzzlePiece {
 
 	private int topLeft, topRight, bottomLeft, bottomRight;
+	private int[] pieceCoordinates;
 	private int pieceID;
 
     public PuzzlePiece(int topLeft, int topRight, int bottomLeft, int bottomRight, int pieceID) {
@@ -13,4 +14,47 @@ public class PuzzlePiece {
         this.pieceID = pieceID;
     }
 
+    public int getTopLeft() {
+        return topLeft;
+    }
+
+    public void setTopLeft(int topLeft) {
+        this.topLeft = topLeft;
+    }
+
+    public int getTopRight() {
+        return topRight;
+    }
+
+    public void setTopRight(int topRight) {
+        this.topRight = topRight;
+    }
+
+    public int getBottomLeft() {
+        return bottomLeft;
+    }
+
+    public void setBottomLeft(int bottomLeft) {
+        this.bottomLeft = bottomLeft;
+    }
+
+    public int getBottomRight() {
+        return bottomRight;
+    }
+
+    public void setBottomRight(int bottomRight) {
+        this.bottomRight = bottomRight;
+    }
+
+    public int getPieceID() {
+        return pieceID;
+    }
+
+    public void setPieceID(int pieceID) {
+        this.pieceID = pieceID;
+    }
+
+    public int[] getPieceCoordinates() {
+        return pieceCoordinates;
+    }
 }
---
+++ src/main/java/com/jigsaw/impl/PuzzlePieceValidator.java
@@ -0,0 +1,26 @@
+package com.jigsaw.impl;
+
+public class PuzzlePieceValidator {
+
+    //Private class members
+    PuzzlePiece piece;
+
+    public boolean isValidPiece(PuzzlePiece pieceToValidate){
+        int size;
+        size =  pieceToValidate.getPieceCoordinates().length;
+        if (size>4 || size<=0){
+            return false; // We got wrong amount of coordinates
+        }
+        int topLeft = pieceToValidate.getTopLeft();
+        int topRight = pieceToValidate.getTopRight();
+        int bottomLeft = pieceToValidate.getBottomLeft();
+        int bottomRight = pieceToValidate.getBottomRight();
+
+        if (topLeft>1||topLeft<0||topRight>1||topRight<0||bottomLeft>1||bottomLeft<0||bottomRight>1||bottomRight<0){
+            return false;// we gor wrong coordinates
+        }
+        return true;
+    }
+
+
+}
