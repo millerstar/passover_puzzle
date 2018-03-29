81b657612b8346a5037c1fa8e604f90d0c009108
Davidson, Matvey (md9897) <md9897@intl.att.com>
2018-03-29 13:22:45 +0300

    updated puzzle piece file

--- src/main/java/com/jigsaw/impl/PuzzlePiece.java
+++ src/main/java/com/jigsaw/impl/PuzzlePiece.java
@@ -2,59 +2,15 @@ package com.jigsaw.impl;
 
 public class PuzzlePiece {
 
-	private int topLeft, topRight, bottomLeft, bottomRight;
-	private int[] pieceCoordinates;
+	private int sideLeft, sideRight, sideBottom, sideTop;
+	private boolean edgeTopLeftIs90, edgeTopRight90, edgeBottomLeftIs90, edgeBottomRight90;
 	private int pieceID;
 
-    public PuzzlePiece(int topLeft, int topRight, int bottomLeft, int bottomRight, int pieceID) {
-        this.topLeft = topLeft;
-        this.topRight = topRight;
-        this.bottomLeft = bottomLeft;
-        this.bottomRight = bottomRight;
+    public PuzzlePiece(int sideLeft, int sideRight, int sideBottom, int sideTop, int pieceID) {
+        this.sideLeft = sideLeft;
+        this.sideRight = sideRight;
+        this.sideBottom = sideBottom;
+        this.sideTop = sideTop;
         this.pieceID = pieceID;
-    }
-
-    public int getTopLeft() {
-        return topLeft;
-    }
-
-    public void setTopLeft(int topLeft) {
-        this.topLeft = topLeft;
-    }
-
-    public int getTopRight() {
-        return topRight;
-    }
-
-    public void setTopRight(int topRight) {
-        this.topRight = topRight;
-    }
-
-    public int getBottomLeft() {
-        return bottomLeft;
-    }
-
-    public void setBottomLeft(int bottomLeft) {
-        this.bottomLeft = bottomLeft;
-    }
-
-    public int getBottomRight() {
-        return bottomRight;
-    }
-
-    public void setBottomRight(int bottomRight) {
-        this.bottomRight = bottomRight;
-    }
-
-    public int getPieceID() {
-        return pieceID;
-    }
-
-    public void setPieceID(int pieceID) {
-        this.pieceID = pieceID;
-    }
-
-    public int[] getPieceCoordinates() {
-        return pieceCoordinates;
     }
 }
