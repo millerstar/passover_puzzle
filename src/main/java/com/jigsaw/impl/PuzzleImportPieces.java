fdf181102677748b05ec99741878d9af910ef290
Miller, Ilan (im107k) <im107k@intl.att.com>
2018-04-02 02:00:16 +0300

    Adding tests to the PuzzleImportPiecesTest class

--- src/main/java/com/jigsaw/impl/PuzzleImportPieces.java
+++ src/main/java/com/jigsaw/impl/PuzzleImportPieces.java
@@ -1,4 +1,21 @@
 package com.jigsaw.impl;
 
+import java.io.File;
+
 public class PuzzleImportPieces {
+
+    // class members
+    private static String PUZZLE_PIECES_PATH;
+    private static final String inputFileName = "puzzlePiecesFile.txt";
+
+    // getters
+    public static String getPuzzlePiecesPath() {
+        File file = new File("src/main/resources/puzzlePiecesFile.txt");
+        PUZZLE_PIECES_PATH = file.getAbsolutePath();
+        return PUZZLE_PIECES_PATH;
+    }
+
+    public static String getInputFileName() {
+        return inputFileName;
+    }
 }
--- src/test/java/com/jigsaw/impl/PuzzleImportPiecesTest.java
+++ src/test/java/com/jigsaw/impl/PuzzleImportPiecesTest.java
@@ -1,7 +1,36 @@
 package com.jigsaw.impl;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PuzzleImportPiecesTest {
 
+    public Path getFilePath() {
+        Path path = Paths.get(PuzzleImportPieces.getPuzzlePiecesPath());
+        return  path;
+    }
+
+    @Test
+    @DisplayName("Input file exist")
+    void isFileExist() {
+        Path path = getFilePath();
+        assertTrue(Files.exists(path), "The input file '" + PuzzleImportPieces.getInputFileName() + "' was not found!");
+    }
+
+    @Test
+    @DisplayName("Correct file name")
+    void isCorrectFileName() {
+        Path path = getFilePath();
+        String actualFileName = path.getFileName().toString();
+        assertEquals(PuzzleImportPieces.getInputFileName(),actualFileName,"The actual file name '" + actualFileName + "' is incorrect!");
+
+    }
 }
