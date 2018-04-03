d0cea00ec2688311de2290f7bb315cef09455106
Miller, Ilan (im107k) <im107k@intl.att.com>
2018-04-03 11:54:16 +0300

    Adding new tests

--- src/main/java/com/jigsaw/impl/PuzzleImportPieces.java
+++ src/main/java/com/jigsaw/impl/PuzzleImportPieces.java
@@ -1,12 +1,20 @@
 package com.jigsaw.impl;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PuzzleImportPieces {
 
     // class members
     private static String PUZZLE_PIECES_PATH;
     private static final String INPUT_FILE_NAME = "puzzlePiecesFile.txt";
+    private static final Path path = Paths.get(getPuzzlePiecesPath());
+
+    List<PuzzlePiece> puzzlePieces = new ArrayList<>();
+    private String numOfPices;
 
     // getters
     public static String getPuzzlePiecesPath() {
@@ -17,5 +25,13 @@ public class PuzzleImportPieces {
 
     public static String getInputFileName() {
         return INPUT_FILE_NAME;
+    }
+
+    public static Path getPath() {
+        return path;
+    }
+
+    public List<PuzzlePiece> getPuzzlePieces() {
+        return puzzlePieces;
     }
 }
--- src/test/java/com/jigsaw/impl/PuzzleImportPiecesTest.java
+++ src/test/java/com/jigsaw/impl/PuzzleImportPiecesTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,24 +16,60 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PuzzleImportPiecesTest {
 
-    public Path getFilePath() {
-        Path path = Paths.get(PuzzleImportPieces.getPuzzlePiecesPath());
-        return  path;
-    }
-
     @Test
     @DisplayName("Input file exist")
     void isFileExist() {
-        Path path = getFilePath();
+        Path path = PuzzleImportPieces.getPath();
         assertTrue(Files.exists(path), "The input file '" + PuzzleImportPieces.getInputFileName() + "' was not found!");
     }
 
     @Test
     @DisplayName("Correct file name")
     void isCorrectFileName() {
-        Path path = getFilePath();
+        Path path = PuzzleImportPieces.getPath();
         String actualFileName = path.getFileName().toString();
-        assertEquals(PuzzleImportPieces.getInputFileName(),actualFileName,"The actual file name '" + actualFileName + "' is incorrect!");
+        assertEquals(PuzzleImportPieces.getInputFileName(), actualFileName, "The actual file name '" + actualFileName + "' is incorrect!");
+    }
 
+    @Test
+    @DisplayName("Is NumElements appear")
+    void isNumElementsAppear() throws IOException {
+        Path path = PuzzleImportPieces.getPath();
+        BufferedReader reader = Files.newBufferedReader(path, Charset.forName("UTF-8"));
+        String currentLine = null;
+        boolean isEmptyLine;
+        String actualString = null;
+        String expectedVal = "NumElements";
+        while ((currentLine = reader.readLine()) != null) {
+            isEmptyLine = currentLine.isEmpty();
+            currentLine = currentLine.trim();
+            if ((isEmptyLine == false && !currentLine.contains("NumElements"))) {
+                actualString = currentLine.trim();
+                break;
+            }
+            if (!isEmptyLine && currentLine.contains("NumElements")) {
+                System.out.println(currentLine.indexOf("="));
+                actualString = currentLine.substring(0, currentLine.indexOf("="));
+                break;
+            }
+        }
+        assertEquals(expectedVal, actualString, "The '" + expectedVal + "' was not display in the file");
+    }
+
+    @Test
+    @DisplayName("Is NumElements in first line")
+    void isNumElementsFirst() throws IOException {
+        Path path = PuzzleImportPieces.getPath();
+        BufferedReader reader = Files.newBufferedReader(path, Charset.forName("UTF-8"));
+        String currentLine = null;
+        int counter = 1;
+        while ((currentLine = reader.readLine()) != null) {
+            if (currentLine.contains("NumElements")) {
+                break;
+            } else {
+                counter++;
+            }
+        }
+        assertTrue(counter == 1, "The 'NumElements' was not display in the first line");
     }
 }
