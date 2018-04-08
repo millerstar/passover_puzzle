d58ad69f8ca89e2e66d4c19c729080d7ae40ace5
idan <idan006@gmail.com>
2018-04-08 18:46:16 +0300

    Added straightRightLeftPieces,maleRightLeftPieces, femaleRightLeftPieces, removed unused lists, modified PuzzleBox ctor

--- src/main/java/com/jigsaw/impl/PuzzleBox.java
+++ src/main/java/com/jigsaw/impl/PuzzleBox.java
@@ -1,63 +1,67 @@
 package com.jigsaw.impl;
 
+import javafx.scene.control.cell.CheckBoxListCell;
+
 import java.util.ArrayList;
 import java.util.List;
 
 // this class contain pieces sorted by place they should take
 public class PuzzleBox {
-    private List<PuzzlePiece> frameCornerPieces = new ArrayList<>() ;
-    private List<PuzzlePiece> frameSidePieces = new ArrayList<>();
-    private List<PuzzlePiece> innerPieces = new ArrayList<>();
+    private List<PuzzlePiece> straightRightLeftPieces = new ArrayList<>() ;
+    private List<PuzzlePiece> maleRightLeftPieces = new ArrayList<>();
+    private List<PuzzlePiece> femaleRightLeftPieces = new ArrayList<>();
+    private List<PuzzlePiece> allPiecesInBoard = new ArrayList<>();
+
     private int numOfPieces;
 
-    public PuzzleBox(List<PuzzlePiece> pieces){
-        for(PuzzlePiece piece : pieces){
-            if(piece.getSideLeft() == 0 || piece.getSideRight() == 0){
-                if(piece.getSideTop() == 0 || piece.getSideBottom() == 0){
-                    frameCornerPieces.add(piece);
-                }
-                frameSidePieces.add(piece);
-                continue;
-            }
+    public PuzzleBox(List<PuzzlePiece> pieces) {
+        this.allPiecesInBoard = pieces;
+        straightRightLeftPieces = getStraightPiecesGroup();
+        maleRightLeftPieces = getMalePiecesGroup();
+        femaleRightLeftPieces = getFemalePiecesGroup();
 
-            if(piece.getSideTop() == 0 || piece.getSideBottom() == 0){
-                if( piece.getSideLeft() == 0 || piece.getSideRight() == 0){
-                    frameCornerPieces.add(piece);
-                }
-                frameSidePieces.add(piece);
-                continue;
-            }
 
-            innerPieces.add(piece);
-        }
-        this.numOfPieces = frameCornerPieces.size() + frameSidePieces.size() + innerPieces.size();
-    }
-
-    public List<PuzzlePiece> getFrameCornerPieces() {
-        return frameCornerPieces;
-    }
-
-    public void setFrameCornerPieces(List<PuzzlePiece> frameCornerPieces) {
-        this.frameCornerPieces = frameCornerPieces;
-    }
-
-    public List<PuzzlePiece> getFrameSidePieces() {
-        return frameSidePieces;
-    }
-
-    public void setFrameSidePieces(List<PuzzlePiece> frameSidePieces) {
-        this.frameSidePieces = frameSidePieces;
-    }
-
-    public List<PuzzlePiece> getInnerPieces() {
-        return innerPieces;
-    }
-
-    public void setInnerPieces(List<PuzzlePiece> innerPieces) {
-        this.innerPieces = innerPieces;
     }
 
     public int getNumOfPieces() {
         return numOfPieces;
     }
+
+    public List<PuzzlePiece> getMalePiecesGroup(){
+        List<PuzzlePiece> retVal = new ArrayList<PuzzlePiece>();
+
+        for (PuzzlePiece piece :allPiecesInBoard) {
+            if (piece.getSideLeft()==1) {
+                //Adding male piece
+                retVal.add(piece);
+            }
+        }
+        return retVal;
+    }
+
+    public List<PuzzlePiece> getFemalePiecesGroup(){
+        List<PuzzlePiece> retVal = new ArrayList<PuzzlePiece>();
+
+        for (PuzzlePiece piece :allPiecesInBoard) {
+            if (piece.getSideLeft()==-1) {
+                //Adding female piece
+                retVal.add(piece);
+            }
+        }
+        return retVal;
+    }
+
+    public List<PuzzlePiece> getStraightPiecesGroup(){
+        List<PuzzlePiece> retVal = new ArrayList<PuzzlePiece>();
+
+        for (PuzzlePiece piece :allPiecesInBoard) {
+            if (piece.getSideLeft()==0) {
+                //Adding straight piece
+                retVal.add(piece);
+            }
+        }
+        return retVal;
+    }
 }
+
+
