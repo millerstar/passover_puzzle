21d02bf523e6c755f568f51b0ecd581574596d34
idan <idan006@gmail.com>
2018-04-13 09:54:57 +0300

    Added BadisValidPiece to PuzzlePieceValidatorTest

--- src/test/java/com/jigsaw/impl/PuzzlePieceValidatorTest.java
+++ src/test/java/com/jigsaw/impl/PuzzlePieceValidatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PuzzlePieceValidatorTest {
@@ -22,6 +23,15 @@ public class PuzzlePieceValidatorTest {
     @DisplayName("test is piece is null")
     public boolean isPieceNotNull(PuzzlePiece pieceToValidate) {
         return pieceToValidate==null;
+    }
+
+    @Test
+    @DisplayName("When given not valid piece")
+    public void BadisValidPiece() {
+        PuzzlePiece unValidPiece = new PuzzlePiece(1,1,1,1,-1);
+        PuzzlePieceValidator p = new PuzzlePieceValidator();
+
+        assertFalse(p.isValidPiece(unValidPiece));
     }
 }
 
