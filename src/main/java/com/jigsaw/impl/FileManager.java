9df61a2a9f2f439f9323ccbfb63ee15cee1beffb
Miller, Ilan (im107k) <im107k@intl.att.com>
2018-04-22 01:01:56 +0300

    re factor the FileManager class + tests

--- src/main/java/com/jigsaw/impl/FileManager.java
+++ src/main/java/com/jigsaw/impl/FileManager.java
@@ -6,9 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 //TODO interface
 public class FileManager implements IPuzzleDataIO {
@@ -16,9 +14,11 @@ public class FileManager implements IPuzzleDataIO {
     // class members
     private Path importAbsPath;
     private Path exportAbsPath;
+    private List<String> errorArrayList;
 
     // constructor
     public FileManager() {
+        errorArrayList = new ArrayList<>();
         File inputFile = new File("src/main/resources/puzzlePiecesFile.txt");
         File outputFile = new File("src/main/resources/puzzleResultFile.txt");
         this.importAbsPath = Paths.get(inputFile.getAbsolutePath());
@@ -29,6 +29,7 @@ public class FileManager implements IPuzzleDataIO {
         File outputFile = new File("src/main/resources/puzzleResultFile.txt");
         this.importAbsPath = Paths.get(fileFullPath);
         this.exportAbsPath = Paths.get(outputFile.getAbsolutePath());
+        errorArrayList = new ArrayList<>();
     }
 
     // getters
@@ -42,8 +43,16 @@ public class FileManager implements IPuzzleDataIO {
         return exportFileName.toString();
     }
 
+    public List<String> getErrorArrayList() {
+        return errorArrayList;
+    }
+
     // class  service methods
-    public BufferedReader openFile(String fileName) {
+    public void addErrorToList(String errorMessage) {
+        errorArrayList.add(errorMessage);
+    }
+
+    private BufferedReader openFile(String fileName) {
         clearResultFile(); /* clear the output result file */
         BufferedReader reader = null;
         try {
@@ -52,6 +61,7 @@ public class FileManager implements IPuzzleDataIO {
             } else if (fileName.equalsIgnoreCase(this.getExportPuzzleFileName())) {
                 reader = Files.newBufferedReader(this.exportAbsPath, Charset.forName("UTF-8"));
             } else {
+                addErrorToList("File name is not valid");
                 throw new InvalidPathException(fileName, "File name is not valid");
             }
 
@@ -61,7 +71,7 @@ public class FileManager implements IPuzzleDataIO {
         return reader;
     }
 
-    public void closeFile(BufferedReader reader) {
+    private void closeFile(BufferedReader reader) {
         try {
             reader.close();
         } catch (IOException e) {
@@ -92,12 +102,27 @@ public class FileManager implements IPuzzleDataIO {
     private List<String[]> getPieceDetailList() throws IOException {
         BufferedReader reader = openFile(getImportPuzzleFileName());
         String currentLine = null;
+        int rowNumber = 1;
+        int numOfElements = 0;
         List<String[]> pieceDataList = new ArrayList<>();
         while ((currentLine = reader.readLine()) != null) {
             if (currentLine.contains("NumElements")) {
+                // first row content validation
+                if (rowNumber != 1) {
+                    addErrorToList("The 'NumElements' was not display on first line in the data file");
+                }
+                if ((currentLine.indexOf("=") < 10) || (currentLine.indexOf("=", currentLine.indexOf("=") + 1) > 0)) {
+                    addErrorToList("The equal sign was not display as expected");
+                }
+                numOfElements = Integer.parseInt(currentLine.substring(currentLine.indexOf("=") + 1, currentLine.length()).trim());
                 continue;
             }
             pieceDataList.add(currentLine.split(" "));
+            rowNumber++;
+        }
+        closeFile(reader);
+        if (numOfElements != rowNumber - 1) {
+            addErrorToList("The number of puzzle pieces in incorrect");
         }
         return pieceDataList;
     }
@@ -115,6 +140,15 @@ public class FileManager implements IPuzzleDataIO {
         } else {
             // TODO: 4/13/2018  pull errors from aggregator and write to file
             printToFile("No valid solution was found for the puzzle");
+
+            // print errors to file
+            if (!errorArrayList.isEmpty()) {
+                Collections.sort(errorArrayList);
+                for (String error : errorArrayList) {
+                    printToFile(error);
+                    printToFile("\n");
+                }
+            }
         }
     }
 
--- src/test/java/com/jigsaw/impl/FileManagerTest.java
+++ src/test/java/com/jigsaw/impl/FileManagerTest.java
@@ -1,22 +1,46 @@
 package com.jigsaw.impl;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FileManagerTest {
 
     @Test
-    void name() {
-        FileManager fm = new FileManager("C:\\Users\\im107k\\Documents\\Logs\\logcat1.txt");
-//        FileManager fm = new FileManager();
+    @DisplayName("Is import file source exist")
+    void isCorrectFileName() throws IOException {
+        FileManager fileManager = new FileManager();
+        List<PuzzlePiece> puzzlePieceList = fileManager.getPuzzlePieces();
+        assertTrue(!fileManager.getErrorArrayList().contains("File name is not valid"), "The import file source exist");
     }
 
     @Test
-    void printPuzzleResult() {
+    @DisplayName("Is NumElements appear")
+    void isNumElementsAppear() throws IOException {
+        FileManager fileManager = new FileManager();
+        List<PuzzlePiece> puzzlePieceList = fileManager.getPuzzlePieces();
+        assertTrue(!fileManager.getErrorArrayList().contains("The 'NumElements' was not display on first line in the data file"), "The 'NumElements' was not display");
     }
 
     @Test
-    void getPuzzlePieces() {
+    @DisplayName("Is equals sigh exists")
+    void isEqualsSighExists() throws IOException {
+        FileManager fileManager = new FileManager();
+        List<PuzzlePiece> puzzlePieceList = fileManager.getPuzzlePieces();
+        assertTrue(!fileManager.getErrorArrayList().contains("The equal sign was not display as expected"), "The equal sign was not display");
+    }
+
+    @Test
+    @DisplayName("Is number of elements value correct")
+    void isNumElementsValueCorrect() throws IOException {
+        FileManager fileManager = new FileManager();
+        List<PuzzlePiece> puzzlePieceList = fileManager.getPuzzlePieces();
+        assertTrue(!fileManager.getErrorArrayList().contains("The number of puzzle pieces in incorrect"), "The number of puzzle pieces in incorrect");
     }
 }
