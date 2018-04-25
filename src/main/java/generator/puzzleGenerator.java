92b6ef7d49051673f254e77990ed9c50e2d01625
Miller, Ilan (im107k) <im107k@intl.att.com>
2018-04-25 23:17:17 +0300

    adding puzzleGenerator class to new branch

---
+++ src/main/java/generator/puzzleGenerator.java
@@ -0,0 +1,21 @@
+package generator;
+
+
+public class puzzleGenerator {
+    // class members
+    private int rows;
+    private int colunms;
+
+    // constructor
+    public puzzleGenerator(int rows, int colunms) {
+        this.rows = rows;
+        this.colunms = colunms;
+    }
+
+    // default constructor
+    public puzzleGenerator() {
+        this.rows = 2;
+        this.colunms = 2;
+    }
+
+}
