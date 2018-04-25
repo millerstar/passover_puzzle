a392a73bfada0cbd4fbc3cce7fb08355d1008b02
idan <idan006@gmail.com>
2018-04-25 17:34:08 +0300

    Added J unit tests to PuzzlePieceValidator

--- src/test/java/com/jigsaw/impl/PuzzlePieceValidatorTest.java
+++ src/test/java/com/jigsaw/impl/PuzzlePieceValidatorTest.java
@@ -1,5 +1,6 @@
 package com.jigsaw.impl;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,14 +17,77 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PuzzlePieceValidatorTest {
 
-    @Test
-    @DisplayName("test isValidPiece")
-    public void isValidPiece() {
-        PuzzlePiece validPiece = new PuzzlePiece(1,1,1,1,1);
-        PuzzlePieceValidator p = new PuzzlePieceValidator();
-        assertTrue(p.isPieceNotNull(validPiece));
+    private static PuzzlePieceValidator p;
 
+    @BeforeAll
+    public static void initialization(){
+        p = new PuzzlePieceValidator();
     }
 
-}
+    @Test
+    @DisplayName("test isValidPiece") //PASS
+    public void isValidPiece() {
+        PuzzlePiece validPiece = new PuzzlePiece(1,1,1,1,1);
+        assertTrue(p.isValidPiece(validPiece));
+    }
+
+    @Test
+    @DisplayName("test isValidPiece with large number in ID") //PASS
+    public void isValidPieceLongID() {
+        PuzzlePiece validPiece = new PuzzlePiece(1,1,1,1,100000);
+        assertTrue(p.isValidPiece(validPiece));
+    }
+
+    @Test
+    @DisplayName("test isValidPiece - negative - providing wrong piece coordinates") //PASS
+    public void negativeIsValidPieceWrongCoordinates() {
+        PuzzlePiece unValidPiece = new PuzzlePiece(30,1,1,1,1);
+        assertFalse(p.isValidPiece(unValidPiece));
+    }
+
+    @Test
+    @DisplayName("test isValidPiece - negative - providing wrong piece  wrong ID") //PASS
+    public void negativeIsValidPieceWrongID() {
+        PuzzlePiece unValidPiece = new PuzzlePiece(0,1,1,1,-2);
+        assertTrue(p.isValidPiece(unValidPiece));
+    }
+
+    @Test
+    @DisplayName("test isValidPiece - negative - providing wrong piece coordinates and the wrong coordinates ") //PASS
+    public void negativeIsValidPieceWrongIDAndWrongCoordinates() {
+        PuzzlePiece unValidPiece = new PuzzlePiece(-30,1,1,1,-2);
+        assertFalse(p.isValidPiece(unValidPiece));
+    }
+
+    @Test
+    @DisplayName("test isPieceASquareTest - with a square piece") //PASS
+    public void isPieceASquareTest(){
+        PuzzlePiece validPieceSquare = new PuzzlePiece(0,0,0,0,1);
+        assertTrue(p.isPieceASquare(validPieceSquare));
+    }
+
+    @Test
+    @DisplayName("test isPieceASquareTest - providing not a square piece - test number 1  ") //PASS
+    public void negativeIsPieceASquareTest1(){
+        PuzzlePiece validPieceSquare = new PuzzlePiece(1,0,1,0,1);
+        assertFalse(p.isPieceASquare(validPieceSquare));
+    }
+
+    @Test
+    @DisplayName("test isPieceASquareTest - providing not a square piece - test number 2  ") //PASS
+    public void negativeIsPieceASquareTest2(){
+        PuzzlePiece validPieceSquare = new PuzzlePiece(1,0,0,0,1);
+        assertFalse(p.isPieceASquare(validPieceSquare));
+    }
+
+    @Test
+    @DisplayName("test isPieceASquareTest - providing not a square piece - test number 3  ") //PASS
+    public void negativeIsPieceASquareTest3(){
+        PuzzlePiece validPieceSquare = new PuzzlePiece(0,1,0,-1,10);
+        assertFalse(p.isPieceASquare(validPieceSquare));
+    }
+
+
+
+    }
 
